2a0da5303ab6f5a49669b822826d818119d6b8a8
hzkitty <1758225523@qq.com>
2026-06-07 22:26:32 +0800

    v0.9.7

--- docker/push.sh
+++ docker/push.sh
@@ -1,9 +1,9 @@
-docker build -f .\docker\Dockerfile -t="hzkitty/rapid-doc:0.9.6" .
-docker push hzkitty/rapid-doc:0.9.6
+docker build -f .\docker\Dockerfile -t="hzkitty/rapid-doc:0.9.7" .
+docker push hzkitty/rapid-doc:0.9.7
 
 
 
-docker build -f .\docker\DockerfileGPU -t="hzkitty/rapid-doc:0.9.6-gpu" .
-docker push hzkitty/rapid-doc:0.9.6-gpu
+docker build -f .\docker\DockerfileGPU -t="hzkitty/rapid-doc:0.9.7-gpu" .
+docker push hzkitty/rapid-doc:0.9.7-gpu
 
 
